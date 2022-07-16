b8d48744a09e427dfa73bbf0d6be57a99916dd8f
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-16 12:49:27 +0200

    Rename to reflect better what it is

--- lib/core/logged-in-user-layout.tsx
+++ lib/core/logged-in-user-layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren, useEffect, useState } from "react";
 import { LogoutButton } from "../auth/logout-button";
 
-export default function BaseLayout({ children }: PropsWithChildren) {
+export default function LoggedInUserLayout({ children }: PropsWithChildren) {
   const [isOnClient, setIsOnClient] = useState(false);
 
   useEffect(() => {
