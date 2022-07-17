c02e02695809afc78386de1f1e58aa362635b1ff
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-17 17:12:11 +0200

    Explain the why behind admin page not redirecting to login

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,7 @@
 export { default } from "next-auth/middleware";
 
-// Pages that will require being logged in:
+// Pages that will require being logged in and redirect to login page if not
+// That does not include pages that might require being logged in (e.g. administration page,
+// whatever the path will be), but you don't want to be redirected, to indicate that
+// such a page exists.
 export const config = { matcher: ["/"] };
