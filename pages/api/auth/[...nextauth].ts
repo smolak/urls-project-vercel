94b648713120ffbb78e521ab6e67fabceea2a052
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-18 23:42:04 +0200

    Store role and image data in token and in session
    I need it to display who is logged in and in what role.
    I might need to have the admin app built separately at some point
    (security wise).

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, User } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../prisma/index";
 import { createCredentialsProvider } from "../../../lib/auth/createCredentialsProvider";
@@ -10,6 +10,27 @@ export const nextAuthOptions: NextAuthOptions = {
   },
   pages: {
     signIn: "/auth/login",
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account?.type === "credentials" && user !== undefined) {
+        token.role = user.role;
+        token.image = user.image;
+      }
+
+      return token;
+    },
+    async session({ session, token, user }) {
+      if (token.role) {
+        session.user.role = token.role as User["role"];
+      }
+
+      if (token.image) {
+        session.user.image = token.image as User["image"];
+      }
+
+      return session;
+    },
   },
   providers: [createCredentialsProvider(prisma, "credentials-login")],
 };
