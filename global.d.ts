6a20f79fc9b8754f505ceed20c3d29f3c72bf1d0
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-18 23:39:17 +0200

    Extend models with data I'll need

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,12 @@
 import "jest-extended";
+import { DefaultUser, DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    role: "user" | "admin";
+  }
+
+  interface Session {
+    user: User & DefaultSession["user"];
+  }
+}
