504f07e2192dba216fe44c80b39599a75776f3a7
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-18 23:43:16 +0200

    Add initial admin panel

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,21 +1,42 @@
 import { nextAuthOptions } from "../api/auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { NextPageWithLayout } from "../_app";
+import { AdminLayout } from "./AdminLayout";
+import { User } from "next-auth";
 
-const AdminPage: NextPage = () => {
-  return <h2>Admin page</h2>;
+const AdminPage: NextPageWithLayout<InferGetServerSidePropsType<typeof getServerSideProps>> = ({ user }) => {
+  return (
+    <AdminLayout title="Dashboard" user={user}>
+      <div>Content here</div>
+    </AdminLayout>
+  );
 };
 
 export default AdminPage;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+interface AdminPageProps {
+  user: User;
+}
+
+const normalizeForServerSideProps = (user: User) => {
+  user.image = user.image || null;
+
+  return user;
+};
+
+export const getServerSideProps: GetServerSideProps<AdminPageProps> = async (context) => {
   const session = await unstable_getServerSession(context.req, context.res, nextAuthOptions);
 
-  if (!session) {
+  if (!session || session.user.role !== "admin") {
     return {
       notFound: true,
     };
   }
 
-  return { props: {} };
+  return {
+    props: {
+      user: normalizeForServerSideProps(session.user),
+    },
+  };
 };
