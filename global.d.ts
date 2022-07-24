3683ebf6998300f29b057044ee4e73051cde22b5
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-24 20:40:51 +0200

    Add missing property

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,7 @@ import { DefaultUser, DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface User extends DefaultUser {
     role: "user" | "admin";
+    createdAt: string;
   }
 
   interface Session {
