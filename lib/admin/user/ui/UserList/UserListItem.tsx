0f925a783543bd3458a67de4106b2f3b18ac0941
Jacek Smolak <jacek.smolak@gmail.com>
2022-07-31 18:28:51 +0200

    Render user's ID
    To find the ID or to easily copy it or whatever else the case might be.

--- lib/admin/user/ui/UserList/UserListItem.tsx
+++ lib/admin/user/ui/UserList/UserListItem.tsx
@@ -9,7 +9,11 @@ export const UserListItem: FC<User> = ({ id, email, role, name, image, createdAt
     <div key={id} className="border-t border-gray-200 px-3 py-3 sm:grid sm:grid-cols-10 sm:gap-3 sm:px-6 text-sm">
       <div className="flex items-center col-span-3">
         <UserImage name={name || ""} image={image} />
-        <span className="ml-4">{name}</span>
+        <span className="ml-4">
+          {name}
+          <br />
+          <span className="text-xs text-gray-400">{id}</span>
+        </span>
       </div>
       <span className="flex items-center col-span-3">{email}</span>
       <span className="flex items-center">{role}</span>
