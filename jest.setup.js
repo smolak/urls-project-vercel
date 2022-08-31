851581dc75451a974ac1e912d708c89b1df6d7cb
Jacek Smolak <jacek.smolak@gmail.com>
2022-08-31 21:58:31 +0200

    Mock couple of global things
    nanoid - as it's written in ESM and Jest has problems running it
    console - so that I don't have it here and there (but not .log)

--- jest.setup.js
+++ jest.setup.js
@@ -7,3 +7,18 @@ import "@testing-library/jest-dom/extend-expect";
 import * as matchers from "jest-extended";
 
 expect.extend(matchers);
+
+const generateRandomString = () => (Math.random() + 1).toString(36).substring(2);
+
+jest.mock("nanoid", () => ({
+  customAlphabet: () => jest.fn().mockReturnValue(generateRandomString()),
+}));
+
+global.console = {
+  ...console,
+  // log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
