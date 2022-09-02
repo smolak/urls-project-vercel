f6ecfc421fd51b47d280235b5796176346e23c1c
Jacek Smolak <jacek.smolak@gmail.com>
2022-09-03 00:08:27 +0200

    Add handling Url and UrlQueue ID generation

--- prisma/middlewares/generateModelId.ts
+++ prisma/middlewares/generateModelId.ts
@@ -2,6 +2,8 @@ import { Prisma } from "@prisma/client";
 import { generateUserId } from "../../lib/user/utils/generateUserId";
 import { generateSessionId } from "../../lib/session/utils/generateSessionId";
 import { generateAccountId } from "../../lib/account/utils/generateAccountId";
+import { generateUrlQueueId } from "../../lib/url-queue/utils/generateUrlQueueId";
+import { generateUrlId } from "../../lib/url/utils/generateUrlId";
 
 export const generateModelId: Prisma.Middleware = async (params: Prisma.MiddlewareParams, next) => {
   if (params.action === "create") {
@@ -16,6 +18,14 @@ export const generateModelId: Prisma.Middleware = async (params: Prisma.Middlewa
 
       case "Account":
         params.args.data.id = generateAccountId();
+        break;
+
+      case "UrlQueue":
+        params.args.data.id = generateUrlQueueId();
+        break;
+
+      case "Url":
+        params.args.data.id = generateUrlId();
         break;
     }
   }
