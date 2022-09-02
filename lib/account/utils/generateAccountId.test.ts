7bdb9cdd1039115f2586929e0fb13c3496bc40de
Jacek Smolak <jacek.smolak@gmail.com>
2022-09-03 00:06:44 +0200

    Now that nanoid can be mocked, add tests for ID generators

--- lib/account/utils/generateAccountId.test.ts
+++ lib/account/utils/generateAccountId.test.ts
@@ -1,9 +1,5 @@
 import { ACCOUNT_ID_PREFIX, generateAccountId } from "./generateAccountId";
 
-jest.mock("nanoid", () => ({
-  customAlphabet: () => jest.fn(),
-}));
-
 describe("generateAccountId", () => {
   it("should prefix id with account prefix", () => {
     const id = generateAccountId();
--- lib/session/utils/generateSessionId.test.ts
+++ lib/session/utils/generateSessionId.test.ts
@@ -1,9 +1,5 @@
 import { SESSION_ID_PREFIX, generateSessionId } from "./generateSessionId";
 
-jest.mock("nanoid", () => ({
-  customAlphabet: () => jest.fn(),
-}));
-
 describe("generateSessionId", () => {
   it("should prefix id with session prefix", () => {
     const id = generateSessionId();
