c79479e3343e0fe30b4aa24e7fc9c55eefc8e8c9
Jacek Smolak <jacek.smolak@gmail.com>
2022-09-03 00:08:27 +0200

    Add handling Url and UrlQueue ID generation

--- prisma/middlewares/generateModelId.test.ts
+++ prisma/middlewares/generateModelId.test.ts
@@ -1,12 +1,10 @@
 import { generateModelId } from "./generateModelId";
-import { Prisma } from "../index";
 import { USER_ID_PREFIX } from "../../lib/user/utils/generateUserId";
 import { SESSION_ID_PREFIX } from "../../lib/session/utils/generateSessionId";
 import { ACCOUNT_ID_PREFIX } from "../../lib/account/utils/generateAccountId";
-
-jest.mock("nanoid", () => ({
-  customAlphabet: () => jest.fn(),
-}));
+import { URL_QUEUE_ID_PREFIX } from "../../lib/url-queue/utils/generateUrlQueueId";
+import { URL_ID_PREFIX } from "../../lib/url/utils/generateUrlId";
+import { Prisma } from "@prisma/client";
 
 describe("generateModelId middleware", () => {
   describe('for "create" action', () => {
@@ -88,6 +86,62 @@ describe("generateModelId middleware", () => {
             data: {
               ...params.args.data,
               id: expect.toStartWith(ACCOUNT_ID_PREFIX),
+            },
+          },
+        });
+      });
+    });
+
+    describe('when model is "UrlQueue"', () => {
+      it("should generate ID prefixed for UrlQueue model", async () => {
+        const params = {
+          action: "create",
+          model: "UrlQueue",
+          args: {
+            data: {
+              // any url queue data
+            },
+          },
+        } as Prisma.MiddlewareParams;
+        const nextSpy = jest.fn();
+
+        await generateModelId(params, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledWith({
+          ...params,
+          args: {
+            ...params.args,
+            data: {
+              ...params.args.data,
+              id: expect.toStartWith(URL_QUEUE_ID_PREFIX),
+            },
+          },
+        });
+      });
+    });
+
+    describe('when model is "Url"', () => {
+      it("should generate ID prefixed for Url model", async () => {
+        const params = {
+          action: "create",
+          model: "Url",
+          args: {
+            data: {
+              // any url data
+            },
+          },
+        } as Prisma.MiddlewareParams;
+        const nextSpy = jest.fn();
+
+        await generateModelId(params, nextSpy);
+
+        expect(nextSpy).toHaveBeenCalledWith({
+          ...params,
+          args: {
+            ...params.args,
+            data: {
+              ...params.args.data,
+              id: expect.toStartWith(URL_ID_PREFIX),
             },
           },
         });
