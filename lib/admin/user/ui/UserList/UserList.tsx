22d00f8892011e362d01748fd0424e86c25f8d1a
Jacek Smolak <jacek.smolak@gmail.com>
2022-09-03 18:57:50 +0200

    Fix showing dropdown menu

--- lib/admin/user/ui/UserList/UserList.tsx
+++ lib/admin/user/ui/UserList/UserList.tsx
@@ -8,7 +8,7 @@ interface UserListProps {
 
 export const UserList: FC<UserListProps> = ({ users }) => {
   return (
-    <div className="bg-white overflow-hidden sm:rounded-lg border border-gray-300 text-gray-900">
+    <div className="bg-white sm:rounded-lg border border-gray-300 text-gray-900">
       <div className="bg-gray-100 px-3 py-3 sm:grid sm:grid-cols-10 sm:gap-3 sm:px-6 font-bold">
         <span className="pl-12 col-span-3">Name</span>
         <span className="col-span-3">Email</span>
