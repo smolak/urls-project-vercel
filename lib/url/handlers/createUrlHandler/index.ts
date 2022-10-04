70381a08f5016526b5379bcf3327487185b20f68
Jacek Smolak <jacek.smolak@gmail.com>
2022-10-04 21:46:19 +0200

    Have more strict typing

--- lib/url/handlers/createUrlHandler/index.ts
+++ lib/url/handlers/createUrlHandler/index.ts
@@ -2,7 +2,14 @@ import { getToken } from "next-auth/jwt";
 import { createUrlHandlerFactory } from "./factory";
 import { triggerEvent } from "../../../events-aggregator/triggerEvent";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Url, UrlQueue } from "@prisma/client";
 
-export type CreateUrlHandler = (req: NextApiRequest, res: NextApiResponse) => Promise<void>;
+type UrlAlreadyExists = { url: Url["url"] };
+type UrlAddedToQueue = { urlQueueId: UrlQueue["id"] };
+type SomethingWentWrong = { error: string };
+
+type ApiResponse = UrlAlreadyExists | UrlAddedToQueue | SomethingWentWrong;
+
+export type CreateUrlHandler = (req: NextApiRequest, res: NextApiResponse<ApiResponse>) => Promise<void>;
 
 export const createUrlHandler: CreateUrlHandler = createUrlHandlerFactory({ getToken, triggerEvent });
