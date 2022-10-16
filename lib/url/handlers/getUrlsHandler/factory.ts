1dc3d81cb2debbdabcb6610dd410633cd02ab594
Jacek Smolak <jacek.smolak@gmail.com>
2022-10-16 22:41:42 +0200

    Add handler for getting urls
    For admin role first.

---
+++ lib/url/handlers/getUrlsHandler/factory.ts
@@ -0,0 +1,49 @@
+import prisma from "../../../../prisma";
+import { CompressedMetadata, decompressMetadata } from "../../../metadata/compression";
+import { StatusCodes } from "http-status-codes";
+import { GetUrlsHandler } from "./index";
+import { GetToken } from "../../../auth/models/GetToken";
+
+interface Params {
+  getToken: GetToken;
+}
+
+export type GetUrlsHandlerFactory = ({ getToken }: Params) => GetUrlsHandler;
+
+export const getUrlsHandlerFactory: GetUrlsHandlerFactory =
+  ({ getToken }) =>
+  async (req, res) => {
+    const token = await getToken({ req });
+
+    if (!token || token?.role !== "admin") {
+      res.status(StatusCodes.FORBIDDEN);
+      return;
+    }
+
+    const userUrls = await prisma.userUrl.findMany({
+      include: {
+        url: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+
+    const urlList = userUrls.map(({ url, user, ...userUrl }) => {
+      return {
+        user,
+        userUrl,
+        url: {
+          ...url,
+          metadata: decompressMetadata(url.metadata as CompressedMetadata),
+        },
+      };
+    });
+
+    res.status(200);
+    res.json(urlList);
+  };
