c12a49cde8713305de3c909f4083d776214a6f4a
Jacek Smolak <jacek.smolak@gmail.com>
2022-10-16 22:45:08 +0200

    Allow fetching urls

--- pages/api/url/index.ts
+++ pages/api/url/index.ts
@@ -2,17 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { StatusCodes } from "http-status-codes";
 
 import { createUrlHandler } from "../../../lib/url/handlers/createUrlHandler";
+import { getUrlsHandler } from "../../../lib/url/handlers/getUrlsHandler";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
 
   switch (method) {
+    case "GET":
+      await getUrlsHandler(req, res);
+      break;
+
     case "POST":
       await createUrlHandler(req, res);
-
       break;
+
     default:
-      res.setHeader("Allow", ["POST"]);
+      res.setHeader("Allow", ["GET", "POST"]);
       res.status(StatusCodes.METHOD_NOT_ALLOWED).end(`Method ${method} Not Allowed`);
   }
 }
