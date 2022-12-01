67e77b7dd766e982a629ca92aae68653f2783af7
Jacek Smolak <jacek.smolak@gmail.com>
2022-12-01 21:37:11 +0100

    Make sure role is on User model and session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../prisma/index";
+import { User } from "@prisma/client";
 
 export const nextAuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -10,6 +11,27 @@ export const nextAuthOptions: NextAuthOptions = {
   },
   pages: {
     signIn: "/auth/login",
+  },
+  callbacks: {
+    async jwt({ token, user, account }) {
+      const { role } = await prisma.user.findFirstOrThrow({
+        where: {
+          id: token.sub,
+        },
+        select: {
+          role: true,
+        },
+      });
+
+      token.role = role;
+
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user.role = token.role as User["role"];
+
+      return session;
+    },
   },
   providers: [
     GithubProvider({
