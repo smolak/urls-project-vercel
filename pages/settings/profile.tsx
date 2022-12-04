f8eb5283ff4988ee18ec153548be12bf824fe24a
Jacek Smolak <jacek.smolak@gmail.com>
2022-12-04 11:20:30 +0100

    Add page for profile settings

---
+++ pages/settings/profile.tsx
@@ -0,0 +1,12 @@
+import { SettingsProfileForm } from "../../lib/settings/profile/ui/SettingsProfileForm";
+
+const SettingsProfile = () => {
+  return (
+    <>
+      <h2>Profile editing page</h2>
+      <SettingsProfileForm />
+    </>
+  );
+};
+
+export default SettingsProfile;
