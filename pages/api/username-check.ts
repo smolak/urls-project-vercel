334d68dc42456e1cb50c4fd5167649d1ae014b1c
Jacek Smolak <jacek.smolak@gmail.com>
2022-12-04 11:20:04 +0100

    Add API for username check

---
+++ pages/api/username-check.ts
@@ -0,0 +1,19 @@
+import type { NextApiHandler } from "next";
+import { StatusCodes } from "http-status-codes";
+import { usernameCheckHandler } from "../../lib/user/handlers/usernameCheckHandler";
+
+const handler: NextApiHandler = async (req, res) => {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      await usernameCheckHandler(req, res);
+      break;
+
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(StatusCodes.METHOD_NOT_ALLOWED).end(`Method ${method} Not Allowed`);
+  }
+};
+
+export default handler;
