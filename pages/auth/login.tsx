38a36ac204216c752ef659bf8545b9708caa62b2
Jacek Smolak <jacek.smolak@gmail.com>
2022-12-09 21:17:57 +0100

    Fix redirecting to user profile data update

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -2,6 +2,7 @@ import { useSession, signIn } from "next-auth/react";
 import { BsGithub } from "react-icons/bs";
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { useEffect } from "react";
 
 const providers = [
   {
@@ -12,63 +13,61 @@ const providers = [
 ];
 
 const Login = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const { push } = useRouter();
 
-  if (status === "loading") return <div>Checking Authentication...</div>;
-
-  if (session) {
-    if (session.user.role === "NEW_USER") {
-      return push("/settings/profile");
+  useEffect(() => {
+    if (session) {
+      if (session.user.role === "NEW_USER") {
+        push("/settings/profile");
+      } else {
+        push("/");
+      }
     }
-
-    setTimeout(() => {
-      push("/");
-    }, 3000);
-
-    return <h2>You are signed in and will be redirected to homepage now.</h2>;
-  }
+  }, [session]);
 
   const handleOAuthSignIn = (provider: string) => () => signIn(provider);
 
   return (
-    <>
-      <Head>
-        <title>Login</title>
-      </Head>
-      <div className="flex justify-center items-center h-screen">
-        <div className="min-h-full max-w-md flex flex-col py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md space-y-8">
-            <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login to your account</h2>
+    !session && (
+      <>
+        <Head>
+          <title>Login</title>
+        </Head>
+        <div className="flex justify-center items-center h-screen">
+          <div className="min-h-full max-w-md flex flex-col py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md space-y-8">
+              <div>
+                <img
+                  className="mx-auto h-12 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                  alt="Workflow"
+                />
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Login to your account</h2>
+              </div>
             </div>
-          </div>
-          <div className="mt-8 space-y-6">
-            {providers.map(({ displayName, name, Icon }) => (
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm
+            <div className="mt-8 space-y-6">
+              {providers.map(({ displayName, name, Icon }) => (
+                <button
+                  type="submit"
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm
                 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
                 focus:ring-offset-2 focus:ring-indigo-500
                 disabled:bg-gray-500 disabled:hover:bg-gray-700 disabled:cursor-not-allowed"
-                key={name}
-                onClick={handleOAuthSignIn(name)}
-              >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                  <Icon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                </span>
-                Login with {displayName}
-              </button>
-            ))}
+                  key={name}
+                  onClick={handleOAuthSignIn(name)}
+                >
+                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                    <Icon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                  </span>
+                  Login with {displayName}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 };
 
