3e186f934a4086d23f91ec8f067834df97cf0d29
Jacek Smolak <jacek.smolak@gmail.com>
2022-12-17 23:11:20 +0100

    Add settings pages

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,14 +1,13 @@
 import { Page, SettingsLayout } from "../../lib/settings/ui/SettingsLayout";
 import { useSession } from "next-auth/react";
-import { UserProfileDataForm } from "../../lib/settings/profile/ui/UserProfileDataForm";
 
 const SettingsProfile = () => {
   const { data: session, status } = useSession();
 
   return (
     status === "authenticated" && (
-      <SettingsLayout title="Profile" user={session.user} page={Page.PROFILE}>
-        <UserProfileDataForm userRole={session.user.role} />
+      <SettingsLayout title="Settings" user={session.user} page={Page.SETTINGS}>
+        To be added...
       </SettingsLayout>
     )
   );
