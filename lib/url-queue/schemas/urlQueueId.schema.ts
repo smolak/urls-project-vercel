ce6f54af67990a1f2c6b99b17884a917c2ff7ae6
Jacek Smolak <jacek.smolak@gmail.com>
2023-01-02 19:11:24 +0100

    You want to trim first, check later ;)

--- lib/url-queue/schemas/urlQueueId.schema.ts
+++ lib/url-queue/schemas/urlQueueId.schema.ts
@@ -4,6 +4,6 @@ import { DEFAULT_ID_SIZE } from "../../shared/utils/generateId";
 
 export const urlQueueIdSchema = z
   .string()
+  .trim()
   .startsWith(URL_QUEUE_ID_PREFIX, { message: "ID passed is not a url queue ID." })
-  .length(URL_QUEUE_ID_PREFIX.length + DEFAULT_ID_SIZE, { message: "Wrong ID size." })
-  .trim();
+  .length(URL_QUEUE_ID_PREFIX.length + DEFAULT_ID_SIZE, { message: "Wrong ID size." });
