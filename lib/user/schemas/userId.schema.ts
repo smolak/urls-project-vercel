daac6c146d23c3b6c15f9a56389c8ecc3ab34c1f
Jacek Smolak <jacek.smolak@gmail.com>
2023-01-02 19:11:24 +0100

    You want to trim first, check later ;)

--- lib/user/schemas/userId.schema.ts
+++ lib/user/schemas/userId.schema.ts
@@ -4,6 +4,6 @@ import { DEFAULT_ID_SIZE } from "../../shared/utils/generateId";
 
 export const userIdSchema = z
   .string()
+  .trim()
   .startsWith(USER_ID_PREFIX, { message: "ID passed is not a user ID." })
-  .length(USER_ID_PREFIX.length + DEFAULT_ID_SIZE, { message: "Wrong ID size." })
-  .trim();
+  .length(USER_ID_PREFIX.length + DEFAULT_ID_SIZE, { message: "Wrong ID size." });
