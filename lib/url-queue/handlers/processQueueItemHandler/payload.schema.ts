16f519943ad6ec00faacf22907583935a32e55ba
Jacek Smolak <jacek.smolak@gmail.com>
2023-01-02 18:51:09 +0100

    Extract urlQueueId schema

--- lib/url-queue/handlers/processQueueItemHandler/payload.schema.ts
+++ lib/url-queue/handlers/processQueueItemHandler/payload.schema.ts
@@ -1,7 +1,8 @@
 import z from "zod";
+import { urlQueueIdSchema } from "../../schemas/urlQueueId.schema";
 
 export type ProcessQueueItemHandlerPayloadSchema = z.infer<typeof processQueueItemHandlerPayloadSchema>;
 
 export const processQueueItemHandlerPayloadSchema = z.object({
-  urlQueueId: z.string().trim(),
+  urlQueueId: urlQueueIdSchema,
 });
