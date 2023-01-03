4bc2344f87448fbf5c32181bff33c9361eaf0d0e
Jacek Smolak <jacek.smolak@gmail.com>
2023-01-03 22:04:44 +0100

    Found a better name for the column

--- test/fixtures/userProfileData.ts
+++ test/fixtures/userProfileData.ts
@@ -1,8 +1,8 @@
-import { UserProfileData } from "@prisma/client";
+import { generateId } from "../../lib/shared/utils/generateId";
 import { generateUserProfileDataId } from "../../lib/user-profile-data/utils/generateUserProfileDataId";
 import { normalizeUsername } from "../../lib/user-profile-data/utils/normalizeUsername";
 import { generateUserId } from "../../lib/user/utils/generateUserId";
-import { generateId } from "../../lib/shared/utils/generateId";
+import { UserProfileData } from "@prisma/client";
 
 export const createUserProfileData = (overwrites: Partial<UserProfileData> = {}): UserProfileData => {
   const username = overwrites.username || "Jacek";
@@ -16,7 +16,7 @@ export const createUserProfileData = (overwrites: Partial<UserProfileData> = {})
     usernameNormalized: normalizeUsername(username),
     userId: generateUserId(),
     following: 0,
-    followedBy: 0,
+    followers: 0,
     ...overwrites,
   };
 };
