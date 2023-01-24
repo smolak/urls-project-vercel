a15f6767de376174cbaa540995eed5d093d4f357
Jacek Smolak <jacek.smolak@gmail.com>
2023-01-24 21:19:11 +0100

    Add API endpoint

---
+++ pages/api/feed-queue/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { StatusCodes } from "http-status-codes";
+import { processFeedQueueItemHandler } from "../../../lib/feed-queue/handlers/processFeedQueueItemHandler";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      await processFeedQueueItemHandler(req, res);
+      break;
+
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(StatusCodes.METHOD_NOT_ALLOWED).end(`Method ${method} Not Allowed`);
+  }
+}
