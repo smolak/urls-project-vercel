673de5fad0a5146d81860c2cb745f07651c726e0
Jacek Smolak <jacek.smolak@gmail.com>
2023-02-14 22:08:27 +0100

    Add configuration for displaying user's feed list

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   serverRuntimeConfig: {
     feedQueueApiKey: process.env.FEED_QUEUE_API_KEY,
+    userFeedList: {
+      itemsPerFetch: 30,
+    },
   },
 };
 
