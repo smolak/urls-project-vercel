14622616512d588c57d3ded45dd450937b315375
Jacek Smolak <jacek.smolak@gmail.com>
2023-02-17 18:47:30 +0100

    Prepare some basic settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement } from "react";
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Session } from "next-auth";
+import { trpc } from "../trpc/utils";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+  getLayout?: (page: ReactElement) => ReactElement;
 };
 
 type AppPropsWithLayout = AppProps<{
@@ -19,7 +20,7 @@ type AppPropsWithLayout = AppProps<{
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
+  const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
 
   return getLayout(
     <QueryClientProvider client={queryClient}>
@@ -30,4 +31,4 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
   );
 }
 
-export default MyApp;
+export default trpc.withTRPC(MyApp);
