4a452d51e001a542bcadcf899f2a262c653a36ad
Jacek Smolak <jacek.smolak@gmail.com>
2023-03-24 23:24:14 +0100

    Work a bit on login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -41,20 +41,10 @@ const Login = () => {
                 <h2 className="mt-6 text-3xl font-bold">Login to your account</h2>
               </div>
             </div>
-            <div className="mt-8 space-y-6">
+            <div className="mt-8 space-y-6 flex justify-center">
               {providers.map(({ displayName, name, Icon }) => (
-                <button
-                  type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm
-                font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
-                focus:ring-offset-2 focus:ring-indigo-500
-                disabled:bg-gray-500 disabled:hover:bg-gray-700 disabled:cursor-not-allowed"
-                  key={name}
-                  onClick={handleOAuthSignIn(name)}
-                >
-                  <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                    <Icon className="h-6 w-6 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
-                  </span>
+                <button type="submit" className="btn btn-primary gap-2" key={name} onClick={handleOAuthSignIn(name)}>
+                  <Icon size={20} aria-hidden="true" />
                   Login with {displayName}
                 </button>
               ))}
