95a593c4907237dfc28d8b80a04e25371761dcd4
Jacek Smolak <jacek.smolak@gmail.com>
2023-04-08 19:24:08 +0200

    Add handler for adding a url using api key

---
+++ lib/url/api/add-url-with-api-key/factory.ts
@@ -0,0 +1,123 @@
+import { StatusCodes } from "http-status-codes";
+import { addUrlWithApiKeyHandlerBodyPayloadSchema } from "./body-payload.schema";
+import { sha1 } from "../../../crypto/sha1";
+import prisma from "../../../../prisma";
+import { EventType, TriggerEvent } from "../../../events-aggregator/trigger-event";
+import { AddUrlWithApiKeyHandler } from "./index";
+import { ID_PLACEHOLDER_REPLACED_BY_ID_GENERATOR } from "../../../../prisma/middlewares/generate-model-id";
+import { Logger } from "pino";
+import { generateRequestId } from "../../../request-id/utils/generate-request-id";
+import { apiKeySchema } from "../../../user-profile-data/schemas/user-profile-data.schema";
+
+interface Params {
+  logger: Logger;
+  triggerEvent: TriggerEvent;
+}
+
+type AddUllWithApiKeyFactory = ({ logger, triggerEvent }: Params) => AddUrlWithApiKeyHandler;
+
+const actionType = "addUrlWithApiKeyHandler";
+
+export const addUrlWithApiKeyFactory: AddUllWithApiKeyFactory =
+  ({ logger, triggerEvent }) =>
+  async (req, res) => {
+    const requestId = generateRequestId();
+    logger.info({ requestId, actionType }, "Creating URL with ApiKey initiated.");
+
+    const apiKeyResult = apiKeySchema.safeParse(req.query.apiKey);
+
+    if (!apiKeyResult.success) {
+      logger.error({ requestId, actionType }, "Invalid ApiKey provided.");
+
+      res.status(StatusCodes.FORBIDDEN);
+      res.json({ error: "Invalid ApiKey provided." });
+      return;
+    }
+
+    const apiKey = apiKeyResult.data;
+    const maybeUser = await prisma.userProfileData.findFirst({
+      where: {
+        apiKey,
+      },
+    });
+
+    if (maybeUser === null) {
+      logger.error({ requestId, actionType }, "User identified by ApiKey not found.");
+
+      res.status(StatusCodes.FORBIDDEN);
+      res.json({ error: "Invalid ApiKey provided." });
+      return;
+    }
+
+    const userId = maybeUser.userId;
+    const bodyResult = addUrlWithApiKeyHandlerBodyPayloadSchema.safeParse(req.body);
+
+    if (!bodyResult.success) {
+      logger.error({ requestId, actionType }, "Body validation error.");
+
+      res.status(StatusCodes.NOT_ACCEPTABLE);
+      return;
+    }
+
+    const url = bodyResult.data.url;
+    const urlHash = sha1(url);
+
+    // Check if there's a record for this hash
+    const maybeUrl = await prisma.url.findUnique({
+      where: {
+        urlHash,
+      },
+    });
+
+    try {
+      // If there's one, attempt creating a userUrl entry
+      if (maybeUrl) {
+        // TODO: Perhaps IDEA#4
+        await prisma.userUrl.create({
+          data: {
+            id: ID_PLACEHOLDER_REPLACED_BY_ID_GENERATOR,
+            userId,
+            urlId: maybeUrl.id,
+          },
+        });
+
+        logger.info({ requestId, actionType, url }, "URL exists, added to user.");
+
+        res.status(StatusCodes.CREATED);
+        res.send({ success: true });
+
+        return;
+      }
+
+      // If there isn't one
+      // TODO: IDEA#5
+      const urlInQueue = await prisma.urlQueue.create({
+        data: {
+          id: ID_PLACEHOLDER_REPLACED_BY_ID_GENERATOR,
+          rawUrl: url,
+          rawUrlHash: urlHash,
+          userId,
+        },
+      });
+
+      logger.info({ requestId, actionType, url }, "URL added to queue.");
+
+      await triggerEvent({
+        type: EventType.URL_QUEUE_CREATED,
+        data: {
+          urlQueueId: urlInQueue["id"],
+          requestId,
+        },
+      });
+
+      logger.info({ requestId }, "Success.");
+
+      res.status(StatusCodes.CREATED);
+      res.json({ success: true });
+    } catch (error) {
+      logger.error({ requestId, actionType, error }, "Failed to store the URL.");
+
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+      res.json({ error: "Failed to add the URL, try again." });
+    }
+  };
