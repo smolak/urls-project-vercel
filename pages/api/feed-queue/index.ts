95104b95e790892e6daba05d904b2d09c0413f53
Jacek Smolak <jacek.smolak@gmail.com>
2023-06-05 17:30:49 +0200

    Have queue be POSTed

--- pages/api/feed-queue/index.ts
+++ pages/api/feed-queue/index.ts
@@ -6,12 +6,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { method } = req;
 
   switch (method) {
-    case "GET":
+    case "POST":
       await processFeedQueueItemHandler(req, res);
       break;
 
     default:
-      res.setHeader("Allow", ["GET"]);
+      res.setHeader("Allow", ["POST"]);
       res.status(StatusCodes.METHOD_NOT_ALLOWED).end(`Method ${method} Not Allowed`);
   }
 }
