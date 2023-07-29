6e43094e94448a626d64c278ea7af4f9f740807c
Jacek Smolak <jacek.smolak@gmail.com>
2023-07-29 09:37:53 +0200

    Improve styles

--- lib/feed/ui/error-loading-user-feed.tsx
+++ lib/feed/ui/error-loading-user-feed.tsx
@@ -4,11 +4,11 @@ export const ErrorLoadingUserFeed = () => {
       <h1 className="mb-8 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl md:text-4xl">
         <span className="inline">We couldn&apos;t load the feed data, sorry 😞</span>{" "}
       </h1>
-      <div className="p-4">
-        <p className="mb-2 flex gap-2">
+      <div className="flex flex-col gap-3 p-4">
+        <p className="flex gap-2">
           <span>✅</span> We log those things are aware of the problem.
         </p>
-        <p className="mb-2 flex gap-2">
+        <p className="flex gap-2">
           <span>💡</span> Try refreshing the page. If that won&apos;t work, be patient, we&apos;re most likely working
           on that already.
         </p>
